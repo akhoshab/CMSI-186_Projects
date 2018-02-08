4942b192668435db560da5fba16e3ea9c42a8fe1
akhoshab <35505066+akhoshab@users.noreply.github.com>
2018-02-08 13:30:09 -0800

    Updated HW02

--- homework02/StringStuff.java
+++ homework02/StringStuff.java
@@ -12,14 +12,17 @@ public class StringStuff {
     * @return  boolean which is true if there is a vowel, or false otherwise
     */
     public static boolean containsVowel( String s ) {
-        s = s.toLowerCase();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'y') {
+        String s2 = s.toLowerCase();
+        //s2 = s.toLowerCase(); -- dont fuck with the original param
+
+        for (int i = 0; i < s2.length(); i++) {
+            if (s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i' ||
+            s2.charAt(i) == 'o' || s2.charAt(i) == 'u' || s2.charAt(i) == 'y') {
                 return true;
             }
         }
         return false;
-
+//use the s2 so you dont have to check the cap letters
 
        // return !(s.indexOf('a') < 0 && s.indexOf('e') && s.indexOf('i') < 0 && s.indexOf('o') < 0 &&  s.indexOf('u') < 0 && s.indexOf('y') < 0);
     }
@@ -33,6 +36,18 @@ public class StringStuff {
     * @return  boolean which is true if this a palindrome, or false otherwise
     */
     public static boolean isPalindrome( String s ) {
+// two indexes --> front and back to move toowards each other
+      if (s.length() == 0 || s.length() == 1)
+        return false;
+      int i = 0;
+      int j = s.length() - 1;
+      while (i != j && i < j ) {
+        if (s.charAt(i) == s.charAt(j)) {
+          i++; j--;
+        }
+        else
+          return false;
+      }
         return true;
     }
 
@@ -45,7 +60,13 @@ public class StringStuff {
     * @return  String containing the &quot;even&quot; letters from the input
     */
     public static String evensOnly( String s ) {
-        return new String( "HJHJHJ" );
+      String s2 = "";  //tkaing pieces of the string --> put it into s2 ean empty string
+      for (int i = 0; i < s.length(); i++) {
+        if ((int)s.charAt(i) % 2 == 0) // putting int infront truns it into the ascii value --> so you can check if its divisible by 2
+            s2 = s2 + s.charAt(i); // add it to string
+      }
+      return s2;
+      //  return new String( "HJHJHJ" );
     }
 
     /**
@@ -57,7 +78,13 @@ public class StringStuff {
     * @return  String containing the &quot;odd&quot; letters from the input
     */
     public static String oddsOnly( String s ) {
-        return new String( "IKIKIK" );
+      String s2 = "";
+      for (int i = 0; i < s.length(); i++) {
+        if ((int)s.charAt(i) % 2 == 1)
+            s2 = s2 + s.charAt(i);
+      }
+      return s2;
+      //  return new String( "IKIKIK" );
     }
 
     /**
@@ -68,8 +95,22 @@ public class StringStuff {
     * @return  String containing the &quot;even&quot; letters from the input without duplicates
     */
     public static String evensOnlyNoDupes( String s ) {
-        return new String( "HJ" );
+      Set<Character> h = new LinkedHashSet<Character>();  //--initializing the hashset
+      String s2 = "";  //tkaing pieces of the string --> put it into s2 ean empty string
+
+      for (int i = 0; i < s.length(); i++) {
+        if ((int)s.charAt(i) % 2 == 0) {
+          if (!h.contains(s.charAt(i))) {
+            s2 += s.charAt(i); // a += b means a + b so youre adding the chracter to the s2 string
+            h.add(s.charAt(i));   // a set contains a bunch of characters and you need to add the character to the set so that it can tell you if it has showed up or not yet in the set
+          }
+      }
     }
+      return s2;
+  }
+
+      //  return new String( "HJ" );
+
 
     /**
     * Method to return the characters in a string that correspond to the &quot;ODD&quot; index
@@ -79,8 +120,19 @@ public class StringStuff {
     * @return  String containing the &quot;odd&quot; letters from the input without duplicates
     */
     public static String oddsOnlyNoDupes( String s ) {
-        return new String( "IK" );
+      Set<Character> h = new LinkedHashSet<Character>();  //--initializing the hashset
+      String s2 = "";  //tkaing pieces of the string --> put it into s2 ean empty string
+
+      for (int i = 0; i < s.length(); i++) {
+        if ((int)s.charAt(i) % 2 == 1) {
+          if (!h.contains(s.charAt(i))) {
+            s2 += s.charAt(i); // a += b means a + b so youre adding the chracter to the s2 string
+            h.add(s.charAt(i));   // a set contains a bunch of characters and you need to add the character to the set so that it can tell you if it has showed up or not yet in the set
+          }
+      }
     }
+    return s2;
+  }
 
     /**
     * Method to return the reverse of a string passed as an argument
@@ -89,7 +141,11 @@ public class StringStuff {
     * @return  String containing the reverse of the input string
     */
     public static String reverse( String s ) {
-        return new String( "kculc eht tahw" );
+      String s2 = "";
+      for (int i = s.length() -1; i >= 0; i--) { // start at the end of the string; keep going while i>= 0; decriment i
+        s2 = s2 += s.charAt(i);
+      }
+      return s2;
     }
 
     /**
@@ -121,4 +177,3 @@ public class StringStuff {
         System.out.println( "reverse()          returns: " + reverse( "REHEARSALSZ" ) );
     }
 }
- 
