10c5431ae2fe06663b29149acaec22d3cdb5557b
akhoshab <35505066+akhoshab@users.noreply.github.com>
2018-05-03 16:24:34 -0700

    Update DynamicChangeMaker.java

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -11,44 +11,52 @@
 
  public class DynamicChangeMaker {
      public static void main(String[] args) {
-         if (args.length != 2){
-             System.out.println("you need two arguments!!!");
+        if (args.length != 2){
+             System.out.println("Impossible. You need two arguments!!!");
              return;
             ///2 args coin denoimnations and amount of change ur making
         }
-        int amount = 0;
-        int[] denominations = new int[0];
-        //no negative amounts or denominations, duplicated denominations
-        //rejct bad data
-         try {
-             amount = Integer.parseInt(args[1]);
-             if (amount < 0) {
-                 System.out.println( "No negative amounts!!!" );
-                 return;
-              }
-              String[] dString = args[0].split(",");
-              denominations = new int[dString.length];
-              for (int i = 0; i < denominations.length; i++){
-                  denominations[i] = Integer.parseInt(dString[i]);
-                  if (denominations[i] < 1) {
-                      System.out.println( "No negative denominations!!!" );
-                      return;
-                  }
-              }
-              for (int i = 0; i < denominations.length - 1; i++){
-                  for(int d = i+1; d < denominations.length; d++){
-                      if (denominations[d] == denominations[i] && d != i){
-                          System.out.println("No Duplicate Denominations!!!");
-                      }
-                  }
-              }
+        // int amount = 0;
+        // int[] denominations = new int[0];
+        // //no negative amounts or denominations, duplicated denominations
+        // //rejct bad data
+        //  try {
+        //      amount = Integer.parseInt(args[1]);
+        //      if (amount < 0) {
+        //          System.out.println( "Impossible. No negative amounts!!!" );
+        //          return;
+        //       }
+        //       String[] dString = args[0].split(",");
+        //       denominations = new int[dString.length];
+        //       for (int i = 0; i < denominations.length; i++){
+        //           denominations[i] = Integer.parseInt(dString[i]);
+        //           if (denominations[i] < 1) {
+        //               System.out.println( "Impossible. No negative denominations!!!" );
+        //               return;
+        //           }
+        //       }
+        //       for (int i = 0; i < denominations.length - 1; i++){
+        //           for(int d = i+1; d < denominations.length; d++){
+        //               if (denominations[d] == denominations[i] && d != i){
+        //                   System.out.println("Impossible. No Duplicate Denominations!!!");
+        //               }
+        //           }
+        //       }
+        //
+        //  } catch(NumberFormatException e) {
+        //      System.out.println("Impossible. Can only make change with numbers!!!");
+        //  }
 
-         } catch(NumberFormatException e) {
-             System.out.println("Can only make change with numbers!!!");
+         int amount = Integer.parseInt(args[0]);
+         String[] dString = args[0].split(",");
+         int[] denominations = new int[dString.length];
+         for (int i = 0; i < denominations.length; i++) {
+             denominations[i] = Integer.parseInt(dString[i]);
          }
+
          Tuple change = makeChangeWithDynamicProgramming (denominations, amount);
          if (change.isImpossible()){
-             System.out.println("Its impossible!!!");
+             System.out.println("Impossible. Its impossible!!!");
          } else {
              int coinTotal = change.total();
              System.out.println(amount + " cents can be made with " + coinTotal + " coins!!!");
@@ -57,6 +65,30 @@
     }
 
     public static Tuple makeChangeWithDynamicProgramming(int[] denominations, int amount){
+        // checks
+        if (amount < 0) {
+            return Tuple.IMPOSSIBLE;
+        }
+        for (int i = 0; i < denominations.length; i++) {
+            if (denominations[i] < 1) {
+                return Tuple.IMPOSSIBLE;
+            }
+        }
+        for (int i = 0; i < denominations.length - 1; i++){
+            for(int d = i+1; d < denominations.length; d++){
+                if (denominations[d] == denominations[i] && d != i){
+                    return Tuple.IMPOSSIBLE;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
         //need 2d array, how many rows and collumns
         int rowCount = denominations.length;
         int columnCount = amount + 1;
